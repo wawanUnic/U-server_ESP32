d853d03f4dbab3868da004721f12778586e52504
wawanUnic <92972782+wawanUnic@users.noreply.github.com>
2024-11-05 08:57:48 +0100

    Version 2

--- U-server/secrets.h
+++ U-server/secrets.h
@@ -3,5 +3,16 @@
 #define STAPSK "1231231232023"
 #endif
 
+
 const char *ssid = STASSID;
 const char *passPhrase = STAPSK;
+String newHostName = "wemos";
+String newDnsName = "wemos";
+const char *passwordOTA = "admin";
+
+#define TIMEZONE "CET-1CEST,M3.5.0,M10.5.0/2"
+// Это указание говорит, что стандартное поясное время имеет аббревиатуру CET
+// и оно смещено на 2 часа вперёд (к востоку) от UTC;
+// Летнее время имеет аббревиатуру CEST и смещено вперед от UTC на три часа (это определяется неявно).
+// Часы переводятся на летнее время в последнее воскресенье марта в 2:00 CET,
+// а на зимнее — в последнее воскресенье октября в 3:00 CEST.
